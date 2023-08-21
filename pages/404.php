3a600d8d05075114ca57902415f8fb14572dfaa8
Damián López <lopezrunco@gmail.com>
2023-08-21 13:39:04 +0530

    not found page

---
+++ pages/404.php
@@ -0,0 +1,16 @@
+<section class='not-found'>
+    <article class='container text-center'>
+        <div class="row">
+            <div class="col-lg-4"></div>
+            <div class="col-lg-4"><img width='100%' src='./assets/404.jpg' alt='404' /></div>
+            <div class="col-lg-4"></div>
+            <div class="col-12">
+                <h2>Página no encontrada</h2>
+                <p>La página que busca no existe o ha sido eliminada.</p>
+                <a href="/agroagua-web-php/index.php" class="btn btn-lg button primary-button mt-4">
+                    Volver a Inicio
+                </a>
+            </div>
+        </div>
+    </article>
+</section>

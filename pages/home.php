e5d80f70baf045f557f2142fe91d5dea0e6bd35e
Damián López <lopezrunco@gmail.com>
2023-08-21 09:45:02 +0530

    about us section

--- pages/home.php
+++ pages/home.php
@@ -1,11 +1,14 @@
 <?php
 $hero_data = file_get_contents('config/hero.json');
 $services_data = file_get_contents('config/services.json');
+$about_data = file_get_contents('config/about.json');
 
 $json_hero_data = json_decode($hero_data, true);
 $json_services_data = json_decode($services_data, true);
+$json_about_data = json_decode($about_data, true);
 ?>
 
+<!-- Hero section / Slider -->
 <div class="hero-section">
     <div class='overcolor'>
         <div class="container f-column">
@@ -15,7 +18,7 @@ $json_services_data = json_decode($services_data, true);
                     <h1>' . $json_hero_data['title'] . '<span>' . $json_hero_data['titleSpan'] . '</span></h1>
                     <p>' . $json_hero_data['text'] . '</p>
                 '
-                ?>
+                    ?>
                 <div class='button-group'>
                     <?php
                     echo '
@@ -32,6 +35,7 @@ $json_services_data = json_decode($services_data, true);
     <img src='./assets/border-hero-img.png' alt='Agro agua' />
 </div>
 
+<!-- Preview services section -->
 <section class='services-home-grid'>
     <div class='container'>
         <div class='row content-wrapper'>
@@ -50,6 +54,33 @@ $json_services_data = json_decode($services_data, true);
                 }
             }
             ?>
+        </div>
+    </div>
+</section>
+
+<!-- About us section -->
+<section class='about-home'>
+    <div class='container'>
+        <div class='row content-wrapper'>
+            <div class='col-lg-6 item-wrapper'>
+                <div class='section-title'>
+                    <?php
+                    echo '
+                        <h2 class="title">' . $json_about_data['title'] . '</h2>
+                        <h3 class="subtitle">' . $json_about_data['subtitle'] . '</h3>
+                    '
+                        ?>
+                </div>
+                <p class='featured-text'>
+                    <span class='featured-text-mark'> | </span>
+                    <?php
+                    echo $json_about_data['text']
+                        ?>
+                </p>
+            </div>
+            <div class='col-lg-6 item-wrapper'>
+                <img src='./assets/about.jpg' alt='Agro agua' />
+            </div>
         </div>
     </div>
 </section>
